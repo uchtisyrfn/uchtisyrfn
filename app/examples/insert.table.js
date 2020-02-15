3efeeadeda30da09715a6e975491568a667c2f87
Ervin <ervin.4happy@gmail.com>
2020-02-15 11:52:49 +0700

    commit awal

--- app/examples/insert.table.js
+++ app/examples/insert.table.js
@@ -1,8 +1,9 @@
 const sql = require("../models/db");
-sql.query ("INSERT INTO books VALUES (NULL, 'title', 'desc', 'perpustakaan.png', current_timestamp());", (err, res) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Data berhasil ditambahkan: " + res.affectedRows);
-    }
+sql.query("INSERT INTO books VALUES (NULL, 'title', 'desc', 'perpustakaan.png', current_timestamp());", (err, res) => {
+ if (err) {
+ console.log(err);
+ } else {
+ console.log("Data berhasil ditambahkan: " + res.affectedRows
+);
+ }
 });
--- app/examples/select.data.js
+++ app/examples/select.data.js
@@ -1,8 +1,8 @@
 const sql = require("../models/db");
 sql.query("SELECT * FROM books", (err, res) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log("Data berhasil ditambahkan: " + res.affectedRows);
-    }
+ if (err) {
+ console.log("error: ", err);
+ } else {
+ console.log("result: ", res);
+ }
 });
